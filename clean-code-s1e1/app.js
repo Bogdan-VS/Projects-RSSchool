73ede4bb8ac6ead5b01ed679619043073c2d5bb1
Bogdan Samonov <bogdansamonov@hmail.com>
2021-11-28 21:56:13 +0300

    feat: add classes to created elements in app.js according rule 3.2 in html-and-css.md

--- clean-code-s1e1/app.js
+++ clean-code-s1e1/app.js
@@ -15,6 +15,7 @@ var completedTasksHolder = document.getElementById('completed-tasks') //complete
 //New task list item
 var createNewTaskElement = function (taskString) {
   var listItem = document.createElement('li')
+  listItem.classList.add('container-item')
 
   //input (checkbox)
   var checkBox = document.createElement('input') //checkbx
@@ -31,11 +32,14 @@ var createNewTaskElement = function (taskString) {
 
   label.innerText = taskString
   label.className = 'task'
+  label.classList.add('task-name')
 
   //Each elements, needs appending
   checkBox.type = 'checkbox'
   editInput.type = 'text'
   editInput.className = 'task'
+  editInput.classList.add('task-item')
+  editInput.classList.add('task-disable')
 
   editButton.innerText = 'Edit' //innerText encodes special characters, HTML does not.
   editButton.className = 'edit'
